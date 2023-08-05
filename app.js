f86ded59d09b7690e61e12ec31f45911a9a74951
Damian Skowroński <skowronskid@icloud.com>
2023-08-05 20:00:31 +0200

    authorization added

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const session = require('express-session')
 
 const connectDB = require('./server/config/db')
 
@@ -13,6 +16,16 @@ connectDB();
 
 app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+}));
 
 app.use(express.static('public'));
 
@@ -21,7 +34,8 @@ app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-app.use('/', require('./server/routes/main'))
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 
 
